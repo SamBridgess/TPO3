1a08b9a8aaa4c93f776fe6344e5d6e38198a624e
SamBridgess <ilya.minyaeff@yandex.ru>
2024-04-27 14:52:54 +0300

    removed comments

--- src/test/java/org/example/tests/QuickSearchTest.java
+++ src/test/java/org/example/tests/QuickSearchTest.java
@@ -9,20 +9,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class QuickSearchTest extends BaseTest{
     private static QuickSearchPage page;
 
-    /*
-      System.setProperty("webdriver.chrome.driver", "/home/sam/TPO/TPO3/drivers/chromedriver");
-       // driver = new FirefoxDriver();
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("start-maximized"); // open Browser in maximized mode
-        options.addArguments("disable-infobars"); // disabling infobars
-        options.addArguments("--disable-extensions"); // disabling extensions
-        options.addArguments("--disable-gpu"); // applicable to windows os only
-        options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
-        options.addArguments("--no-sandbox"); // Bypass OS security model
-
-        driver = new ChromeDriver(options);
-        driver.get(url);
-     */
     @BeforeAll
     public static void configureTest(){
         initDriver();
